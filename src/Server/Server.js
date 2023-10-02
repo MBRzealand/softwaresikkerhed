0da3983fc273f4a5459ba8d84d2d5a5d2cf5f14a
MBRZealand <mikk24f6@edu.zealand.dk>
2023-10-02 16:23:36 +0200

    mostly functional

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -2,18 +2,36 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const cors = require("cors");
+app.set('trust proxy', true)
 const port = 5444
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
 
+let fs = require('fs');
+let logStream = fs.createWriteStream('log.txt', {flags: 'a'});
+
+// logStream.write('Initial line...');
+// logStream.end('this is the end line');
+
+const getCurrentDate = () => {
+    let currentdate = new Date();
+    return currentdate.getDate() + "/"
+        + (currentdate.getMonth() + 1) + "/"
+        + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds()
+}
+
 
 let db = new sqlite3.Database('notes.sqlite', (err) => {
     if (err) {
         return console.error(err.message);
     }
-    console.log('Connected to the in-memory SQlite database.');
+    console.log('\nConnected to the in-memory SQlite database.');
+    logStream.write("\nServer started at: " + getCurrentDate())
 });
 
 app.post('/save-note', (req, res) => {
@@ -28,6 +46,7 @@ app.post('/save-note', (req, res) => {
             res.status(500).json({ error: 'Error adding element to the database' });
         } else {
             console.log(`Element added with ID: ${this.lastID}`);
+            logStream.write("\nNote saved by: " + req.ip + " at: " + getCurrentDate())
             res.json({ message: 'Element added to the database' });
         }
     });
@@ -42,6 +61,7 @@ app.get('/get-notes', (req, res) => {
             console.error(err.message);
             res.status(500).json({ error: 'Error retrieving elements from the database' });
         } else {
+            logStream.write("\nNotes fetched by: " + req.ip + " at: " + getCurrentDate())
             res.json({ notes: rows });
         }
     });
@@ -50,13 +70,14 @@ app.get('/get-notes', (req, res) => {
 app.delete('/delete-note/:id', (req, res) => {
     const noteId = req.params.id;
 
-    const deleteQuery = `DELETE FROM notes WHERE number = id`;
+    const deleteQuery = `DELETE FROM notes WHERE number = ?`;
 
     db.run(deleteQuery, [noteId], (err) => {
         if (err) {
             console.error(err.message);
             res.status(500).json({ error: 'Error deleting the element from the database' });
         } else {
+            logStream.write("\nNote number: " + noteId + " deleted by: " + req.ip + " at: " + getCurrentDate())
             res.json({ message: 'Element deleted from the database' });
         }
     });
@@ -75,6 +96,7 @@ app.put('/update-note/:id', (req, res) => {
                 console.error(err.message);
                 res.status(500).json({ error: 'Failed to update note' });
             } else {
+                logStream.write("\nNote number: " + noteId + " updated by: " + req.ip + " at: " + getCurrentDate())
                 res.json({ message: 'Note updated successfully' });
             }
         }
